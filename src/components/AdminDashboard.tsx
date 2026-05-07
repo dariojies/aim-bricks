198db043e44717d2fc1a34f71785ff19c9978b22
Juanmallout4 <jmmarinborrego@gmail.com>
2026-05-07 17:10:01 +0200

    Update AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1772,7 +1772,6 @@ export const AdminDashboard: React.FC<AdminDashboardProps> = ({ user }) => {
                   <div style={{ fontSize: '0.9rem', color: 'var(--text-muted)' }}>Stock Total Acumulado: <span style={{ color: 'var(--text)', fontWeight: 700 }}>{selectedClubStats.totalStock} unidades</span></div>
                 </div>
               </div>
-            </div>
           )}
         </div>
       )}
